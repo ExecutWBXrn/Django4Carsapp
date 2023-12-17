bab5ca32c8c3fa364e8f099965169a1e12b8fd66
ExecutWBXrn <executwbxrn@gmail.com>
2023-12-17 14:06:45 +0000

    add db_data file(not database it just list) + was used if and for in templates

--- carsmaincategory/views.py
+++ carsmaincategory/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+db_data = (
+    {"id":1, "car":"lanos", "year":2007 ,"is_published":True},
+    {"id":2, "car":"Tesla cybertruck", "year":2077 ,"is_published":True},
+    {"id":3, "car":"ME", "year":"2005" ,"is_published":True},
+)
+
 def index(request):
     context = {
         "title" : "lanos.com",
+        "mainmenu" : ["about site", "favorites", "about us", "log in"],
+        "db_data":db_data,
     }
     return render(request, "carsmaincategory/index.html", context=context)
 
