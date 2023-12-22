ac5c187a076bff8a454fb170418441ea65242781
ExecutWBXrn <executwbxrn@gmail.com>
2023-12-22 21:01:44 +0000

    add migrations

--- carsmaincategory/models.py
+++ carsmaincategory/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Cars(models.Model):
+    title = models.CharField(max_length=255)
+    year = models.IntegerField(max_length=4)
+    content = models.TextField(blank=True)
+    data_create = models.DateTimeField(auto_now_add=True)
+    data_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
