121035977adfb54744faa964d765edbdf972a83d
ExecutWBXrn <executwbxrn@gmail.com>
2024-01-13 18:04:25 +0000

    create ForeignKey

--- carsmaincategory/models.py
+++ carsmaincategory/models.py
@@ -10,6 +10,7 @@ class Cars(models.Model):
     data_create = models.DateTimeField(auto_now_add=True)
     data_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
@@ -19,3 +20,11 @@ class Cars(models.Model):
 
     def get_absolute_url(self):
         return reverse("carinfo", kwargs={"car_slug":self.slug})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
