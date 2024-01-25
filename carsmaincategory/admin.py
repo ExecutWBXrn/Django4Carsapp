c08e03d9f0fa0822f6d4bec72e209c99fc93e650
ExecutWBXrn <executwbxrn@gmail.com>
2024-01-25 21:12:39 +0000

    upgrade admin panel

--- carsmaincategory/admin.py
+++ carsmaincategory/admin.py
@@ -1,12 +1,27 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import *
 @admin.register(Cars)
 class CarsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'year','data_update', 'is_published', 'cat']
+    list_display = ['id', 'title', 'year','data_update', 'is_published', 'cat', 'brief_info']
     list_display_links = ['id', 'title']
     ordering = ['data_update', 'title']
     list_editable = ['is_published']
     list_per_page = 5
+    actions = ["set_published", "set_draft"]
+
+    @admin.display(description="коротка інформація", ordering="content")
+    def brief_info(self, car: Cars):
+        return f"overall amount symbols of content:{len(car.content)}"
+    @admin.action(description="publish up")
+    def set_published(self, request, queryset):
+        q = queryset.update(is_published=Cars.Status.PUBLISHED)
+        self.message_user(request, f"Опубліковано {q} машини")
+
+    @admin.action(description="draft up")
+    def set_draft(self, request, queryset):
+        q = queryset.update(is_published=Cars.Status.DRAFT)
+        self.message_user(request, f"Деопубліковано {q} машини", messages.WARNING)
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
